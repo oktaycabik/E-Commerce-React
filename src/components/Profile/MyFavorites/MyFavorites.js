5b2f340279d379ff0a6fa822c49f4215a61d88a0
Oktay Çabik <oktaycabik2@gmail.com>
2022-03-04 15:47:13 +0300

    düzenleme

--- src/components/Profile/MyFavorites/MyFavorites.js
+++ src/components/Profile/MyFavorites/MyFavorites.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+
 import { getProfileById } from "../../../redux/Auth/authSlice";
 
 function MyFavorities() {
